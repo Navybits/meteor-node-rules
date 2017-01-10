87f9720b9fdca88985131d6362a0705e72c14033
taha <taha.ziadeh@navybits.com>
2017-01-10 20:55:42 +0200

    upgraded to 1.0.0

--- package.js
+++ package.js
@@ -3,7 +3,7 @@ Npm.depends({
 });
 Package.describe({
   name: 'navybits:meteor-node-rules',
-  version: '0.0.1',
+  version: '1.0.0',
   // Brief, one-line summary of the package.
   summary: 'simple wrapper on node-rules npm package made for meteor use',
   // URL to the Git repository containing the source code for this package.
